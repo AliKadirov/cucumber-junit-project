b090793348c0362514fcb7f6ef877dd075a5c7a2
Ali Kodirov <malikodirov@gmail.com>
2022-10-28 10:47:03 -0500

    changed

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -2,6 +2,13 @@ package com.cydeo.utilities;
 
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BrowserUtils {
@@ -38,6 +45,33 @@ public class BrowserUtils {
         String actualTitle = driver.getTitle();
 
         Assert.assertEquals(expectedTitle,actualTitle);
+
+    }
+
+    public static void waitForInvisibilityOf(WebElement element){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),15);
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    /**
+     * This method will accept dropdown as a WebElement
+     * and return all the options' text in a List of String
+     * @return List<String></String>
+     */
+    public static List<String> dropdownOptionsAsString(WebElement dropdown){
+        Select select = new Select(dropdown);
+
+        // List of all ACTUAL month <options> as a WebElement
+        List<WebElement> actualOptionsAsWebElement = select.getOptions();
+
+        // List of all ACTUAL month options as a String
+        List<String> actualOptionsAsString = new ArrayList<>();
+
+        // with using for loop we will convert each WebElement of options to String with using getText() method
+        // with using add() method we will add each String option in List<String> actual options as a String
+        for (WebElement each : actualOptionsAsWebElement) {
+            actualOptionsAsString.add(each.getText());
+        }
 
     }
 
