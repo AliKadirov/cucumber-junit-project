d09fa6a25da7ab21cb21acf58e08239d45330c37
Ali Kodirov <malikodirov@gmail.com>
2022-11-01 09:37:43 -0500

    A lot of file created

--- src/test/java/com/cydeo/step_definitions/GoogleSearch_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleSearch_StepDefinitions.java
@@ -7,53 +7,51 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-
-import java.security.Key;
 
 public class GoogleSearch_StepDefinitions {
 
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
+
     @Given("user is on Google search page")
     public void user_is_on_google_search_page() {
 
-        Driver.getDriver().get("https://www.google.com/");
+        Driver.getDriver().get("https://www.google.com");
 
     }
 
-    @When("user types apple in the google search box and click enter")
-    public void user_types_apple_in_the_google_search_box_and_click_enter() {
 
-        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
+    @When("user types apple in the google search box and clicks enter")
+    public void user_types_apple_in_the_google_search_box_and_clicks_enter() {
+
+        googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
 
     }
 
-    @Then("user sees apple - Google Search is in the google title")
+
+    @Then("user sees apple – Google Search is in the google title")
     public void user_sees_apple_google_search_is_in_the_google_title() {
-
         String expectedTitle = "apple - Google Search";
         String actualTitle = Driver.getDriver().getTitle();
-        Assert.assertEquals(actualTitle, expectedTitle);
 
+        Assert.assertEquals("Title verification is failed!",expectedTitle,actualTitle);
     }
 
 
-    @When("user types {string} in the google search box and click enter")
-    public void userTypesInTheGoogleSearchBoxAndClickEnter(String searchKeyword) {
+    @When("user types {string} in the google search box and clicks enter")
+    public void user_types_in_the_google_search_box_and_clicks_enter(String searchkeyword) {
 
-        googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
+        googleSearchPage.searchBox.sendKeys(searchkeyword + Keys.ENTER);
 
     }
 
     @Then("user sees {string} is in the google title")
-    public void userSeesIsInTheGoogleTitle(String expectedTitle) {
+    public void user_sees_is_in_the_google_title(String expectedTitle) {
 
-        
         String actualTitle = Driver.getDriver().getTitle();
 
-        Assert.assertEquals(actualTitle, expectedTitle);
-
+        Assert.assertEquals(expectedTitle,actualTitle);
 
     }
+
 }
